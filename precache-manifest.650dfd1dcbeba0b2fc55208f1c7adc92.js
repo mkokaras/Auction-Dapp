d95dcb03c1325b6b6d0908dd030742813a86f6ca
mkokaras <mkokaras>
2021-05-31 18:30:05 +0300

    Updates

---
+++ precache-manifest.650dfd1dcbeba0b2fc55208f1c7adc92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "becd077885adbc652c0dfb05c062147d",
+    "url": "/Auction-app/index.html"
+  },
+  {
+    "revision": "f4db2c0032f2b7823f19",
+    "url": "/Auction-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7c40123c22f8222a5de8",
+    "url": "/Auction-app/static/js/2.467bd1e4.chunk.js"
+  },
+  {
+    "revision": "f4db2c0032f2b7823f19",
+    "url": "/Auction-app/static/js/main.099f5cd6.chunk.js"
+  },
+  {
+    "revision": "f7472452af8151a2b3cc",
+    "url": "/Auction-app/static/js/runtime-main.0409cdc2.js"
+  }
+]);
